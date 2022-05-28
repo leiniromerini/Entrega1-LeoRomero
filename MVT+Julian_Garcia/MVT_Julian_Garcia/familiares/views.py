4abbc269b62874af44074c443b3368e9893dd60d
julian1306 <julian_1306@hotmail.com>
2022-05-27 22:24:53 -0300

    context=context

--- MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/views.py
+++ MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/views.py
@@ -33,7 +33,7 @@ def familiares(request):
 
 
     context = { "familiar_nuevo":familiar_nuevo, "familiar_nuevo2":familiar_nuevo2, "familiar_nuevo3":familiar_nuevo3, "familiares_all":familiares_all}
-    return render(request, "familiares.html", context)
+    return render(request, "familiares.html", context=context)
 
 
     
