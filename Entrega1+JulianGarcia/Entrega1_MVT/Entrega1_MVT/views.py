a4c5ca8e6658537738d80cc80f917b071a5508c5
julian1306 <julian_1306@hotmail.com>
2022-06-07 21:32:22 -0300

    commit 2

--- Entrega1+JulianGarcia/Entrega1_MVT/Entrega1_MVT/views.py
+++ Entrega1+JulianGarcia/Entrega1_MVT/Entrega1_MVT/views.py
@@ -5,6 +5,5 @@ from django.http import HttpRequest
 from django.shortcuts import render 
 
 # Pagina Inicio 
-def index(request):                                    # Aca creo una def para la pagina de inicio 
-    return render(request, "index.html")               # con esto se lo mando al index.html el template para esta pagina de inicio en carpeta templates 
-
+def index(request):
+    return render(request, "index.html")          
